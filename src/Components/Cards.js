4560bff9cff0e46bb9b11992803a419d00eed765
hp <57030669+hammad8634@users.noreply.github.com>
2023-05-26 18:29:59 +0500

    card0 hover updates

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './Cards.css'; // Create a separate CSS file for styling
+import React from "react";
+import "./Cards.css"; // Create a separate CSS file for styling
 
 const Card = ({ title, description, image }) => {
-  
   return (
     <div className="card rounded-0 border-0 ms-3 imgsize">
-   <div className='image-container'>
-                            <img src={image} className="card-img-top rounded-0 img-size img-fluid" alt="..." /> </div>
-                            <div className="card-body border-0">
-                            <div className='d-flex justify-content-between'>
-                                <div className='fs-3 mb-4 '><h5>{title}</h5></div>
-                               
-                            </div>
-                            <p className="card-text">{description}</p>
-                        </div>
+      <div className="image-container">
+        <img
+          src={image}
+          className="card-img-top rounded-0 img-size img-fluid"
+          alt="..."
+        />{" "}
+      </div>
+      <div className="card-body border-0">
+        <div className="d-flex justify-content-between">
+          <div className="fs-3 mb-4 ">
+            <h5>{title}</h5>
+          </div>
+        </div>
+        <p className="card-text">{description}</p>
+      </div>
     </div>
   );
 };
--- src/Components/Cards0.js
+++ src/Components/Cards0.js
@@ -1,50 +1,88 @@
-import React from 'react';
-import womenImage from './images/women.png';
-import homeImage from './images/home.png';
-import girlImage from './images/girl.png';
-import './Cards0.css';
-
+import React, { useState } from "react";
+import "./Cards0.css";
+import girlImage from "./images/girl.png";
+import homeImage from "./images/home.png";
+import womenImage from "./images/women.png";
 
 function Cards() {
-    const array = [
-        {
-            id: 1,
-            title: 'Supporting Commuity Health',
-            imag1: womenImage,
-            date: 'May 23, 2022'
-        },
-        {
-            id: 2,
-            title: 'Planning for the future: a new framework for improving health outcomes worldwide',
-            imag1: homeImage,
-            date: 'October 10, 2022'
-        },
-        {
-            id: 3,
-            title: 'Women in leadership: paving the way for equal health',
-            imag1: girlImage,
-            date: 'December 07, 2022'
-        }
-    ]
-    // console.log(array);
+  const array = [
+    {
+      id: 1,
+      title: "Supporting Commuity Health",
+      imag1: womenImage,
+      date: "May 23, 2022",
+      hovertext:
+        "Why helping to bolster primary care services on the ground in Africa is a priority for Roche.",
+    },
+    {
+      id: 2,
+      title:
+        "Planning for the future: a new framework for improving health outcomes worldwide ",
+      imag1: homeImage,
+      date: "October 10, 2022",
+      hovertext:
+        "This collection of stories provides a vision for the future, with tangible examples of how healthcare systems are shifting away from traditional delivery frameworks towards sustainable, resilient pathways that provide better and more efficient care for all",
+    },
+    {
+      id: 3,
+      title: "Women in leadership: paving the way for equal health",
+      imag1: girlImage,
+      date: "December 07, 2022",
+      hovertext:
+        "Greater representation of women in leadership positions can help prioritise women’s health issues, helping to close the data gap and improve their outcomes.",
+    },
+  ];
 
-    return (
-        <div>
-            <div className='d-flex flex-wrap justify-content-lg-between justify-content-center my-5' >
-                {array.map(item => {
-                    return (
-                        <div className="card card3 rounded-0 mt-3  shadow border-0 " key={item.id}>
-                            <img src={item.imag1} className="card-img-top rounded-0 brdr " alt="..." />
-                            <div className="card-body border-0 ">
-                                <p className="card-text fst-italic mb-5">{item.title}</p>
-                                <p className='mb-0'>{item.date}</p>
-                            </div>
-                        </div>
-                    )
-                })}
+  return (
+    <div>
+      <div className="d-flex flex-wrap justify-content-lg-between justify-content-center my-5">
+        {array.map((item) => {
+          const [hovered, setHovered] = useState(false);
+
+          const handleMouseEnter = () => {
+            setHovered(true);
+          };
+
+          const handleMouseLeave = () => {
+            setHovered(false);
+          };
+
+          return (
+            <div
+              className={`card card3 rounded-0 mt-3 shadow border-0 ${
+                hovered ? "hovered" : ""
+              }`}
+              key={item.id}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              {hovered ? (
+                <>
+                  <div className="hover-content">
+                    <h4 className="hover-title">{item.title}</h4>
+                    <p className="hover-text">{item.hovertext}</p>
+                    <p className="hover-date">{item.date}</p>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <img
+                    src={item.imag1}
+                    className="card-img-top rounded-0 brdr"
+                    alt="..."
+                  />
+                  <div className="card-body border-0 pb-5">
+                    <h4 className="card-title">{item.title}</h4>
+                    <p className="card-date ">{item.date}</p>
+                  </div>
+                </>
+              )}
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
